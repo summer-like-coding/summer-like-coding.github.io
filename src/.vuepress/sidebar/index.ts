0db666c890759ea9a0912bb2781079e324d4b889
summer <3244742300@qq.com>
2023-05-05 10:56:43 +0800

    fix: delete demo

--- src/.vuepress/sidebar/index.ts
+++ src/.vuepress/sidebar/index.ts
@@ -3,19 +3,6 @@ import { sidebar } from "vuepress-theme-hope";
 export const Sidebar = sidebar({
   "/": [
     "",
-    {
-      icon: "discover",
-      text: "案例",
-      prefix: "demo/",
-      link: "demo/",
-      children: "structure",
-    },
-    {
-      text: "文档",
-      icon: "note",
-      prefix: "guide/",
-      children: "structure",
-    },
     "slides",
   ],
 });
